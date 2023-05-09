9f8f765f659870c7b0715607832c0a65beb9a985
Sanket Eknath Mahajan <88873453+Sanketmahajan03@users.noreply.github.com>
2023-05-09 23:36:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+/*let fristName = "Sanket";
+let LastName = "Mahajan";
+let Middle_Name = "Eknath";
+
+const number = 99;
+
+var courseName = "Web develvopment";
+var CardNumber = "1234567899";
+var isPaid = true;
+
+var modeOfPayment;
+modeOfPayment = prompt("Mode Of Payment ?")
+
+alert(fristName + " " + LastName +  " Sucessfully bought course of " + courseName + " using " + modeOfPayment);
+
+// code for switch case in javascript.
+
+let day = prompt("Enter your day");
+
+switch(day){
+    case 'monday':
+        console.log("7Am");
+        break;
+    case 'Tuseday':
+        console.log("5Am");
+        break;
+    case 'wendesday':
+        console.log("6Am");
+        break;
+    case 'thrusday':
+        console.day("4Am");
+        break;
+    case 'saturday':
+        console.log("5Am");
+        break;
+    case 'sunday':
+        console.log("10Am");
+        break;
+    default:
+        console.log("invalid Day");
+
+}
+
+
+// objects in javascript;
+// name = 'object'
+//fname and num == object literls.
+let name = {
+    fname: 'sanket',
+    num: 38
+}
+console.log(name);
+
+// array in javascript;
+
+let arrName = ["Sanket","Kundan","Vaishali","Rohit"];
+console.log(arrName[0]);
+console.log(arrName[2]);
+*/
+
+// let x = "my name is ";
+// console.log(x + "Sanket");
+ 
+function sum(a,b){
+    return a+b;
+}
+c = sum(10,20);
+c1 = sum(20,10);
+console.log(c,c1)
